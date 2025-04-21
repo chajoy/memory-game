4c634a65f999026e686b0108dff5ec5b7e8612fa
Charlie Joy <186746025+chajoy@users.noreply.github.com>
2025-04-21 16:54:28 +0100

    Add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [highScore, setHighScore] = useState(0);
+  const [currentScore, setCurrentScore] = useState(0);
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img src="/favicon.svg" alt="dice icon" />
+        </li>
+        <li>Memory Game</li>
+        <li>High Score: {highScore}</li>
+        <li>Current Score: {currentScore}</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default App;
